cf33aa7c072e53ee548c5f6d7a5ed8a0918309a3
Anthony Quispe <anthonyqsalazar@gmail.com>
2024-11-07 17:52:29 -0500

    add design/first design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
-import "./App.css";
+import Button from "./components/button";
+import "./css/global.css";
 
 export default function App() {
-  return <></>;
+  const numberButtons = () => {
+    const buttons = [];
+    for (let i = 9; i > -1; i--) {
+      buttons.push(<Button key={i} value={i} />);
+    }
+    return <div className="number-buttons">{buttons}</div>;
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="calculator">
+          <input type="text" readOnly />
+          <div className="wrapper-buttons">
+            {numberButtons()}
+            <div className="operator-buttons">
+              <Button value="/"></Button>
+              <Button value="x"></Button>
+              <Button value="-"></Button>
+              <Button value="+"></Button>
+              <Button value="="></Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
